3f32a9ede89e25bdfb7af030a228628b86741ccd
Paul Henstridge <PaulHenstridge"gmail.com>
2021-02-13 20:35:53 +0000

    basically working...

---
+++ index.js
@@ -0,0 +1,134 @@
+const express = require('express')
+const socket = require('socket.io')
+
+// App setup
+const app = express()
+const server = app.listen(4000, () => {
+    console.log('server running')
+})
+
+// Static files
+app.use(express.static('public'))
+
+// trying to avoid Cors blocking.  masy have been VS live server issue...?
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
+})
+
+// Socket setup
+const io = socket(server)
+
+
+const activePlayers = []
+
+let dealerHand = []
+let playerHand = []
+let stickingPlayers = []
+
+
+io.on('connection', socket => {
+    console.log(`socket ${socket.id} connected`)
+
+    // Handle chat event
+    socket.on('chat', data => {
+        io.sockets.emit('chat', data)
+    })
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data)
+    })
+
+    // Deal card function
+    function getCard(arr){
+        values = [2,3,4,5,6,7,8,9,10,"J10","Q10","K10","A11"];
+        suits = ["C", "S", "H", "D"];
+
+        let valIndex = Math.floor(Math.random() * 13);
+        let suitIndex = Math.floor(Math.random() * 4);
+
+        arr.push((suits[suitIndex]+ values[valIndex]))
+    }
+
+
+
+    // handle blackjack events
+
+    socket.on ('start', data => {
+        console.log(data.playerName)
+        activePlayers.push(data.playerName) 
+        console.log('active players:' , activePlayers)
+        // now the first person to click is activePlayers[0]
+        // need to remove from the array if disconnected
+        if (activePlayers.length <2) {
+             socket.broadcast.emit('waiting', {opponent: data.playerName})
+        } else {
+        //deal cards into 2 arrays, pass through to Browser            
+            getCard(playerHand)
+            getCard(dealerHand)
+            getCard(playerHand)
+            getCard(dealerHand)
+
+            io.sockets.emit('begin', {playerHand, dealerHand} )
+        }
+    })
+
+    socket.on('twist', data => {
+        let twistCard = []
+        getCard(twistCard)
+        io.sockets.emit('twist', {
+            playerName: data.playerName,
+            twistCard: twistCard
+        })
+    })
+
+
+
+    socket.on('stick', data => {
+        console.log('stick data', data)
+       
+        stickingPlayers.push(data)
+        console.log('sticking players ', stickingPlayers)
+
+
+        function pickWinner(stickingPlayers) {
+            if (stickingPlayers[0].currentScore > stickingPlayers[1].currentScore) return stickingPlayers[0].playerName
+            if (stickingPlayers[0].currentScore === stickingPlayers[1].currentScore) {
+               return "it's a tie!" 
+            } else { 
+                return stickingPlayers[1].playerName
+            }
+        }  
+        
+
+        if (stickingPlayers.length > 1) {
+            let winner = pickWinner(stickingPlayers)
+            console.log('winner is : ', winner)
+            io.sockets.emit('gameOver', {
+                scores : stickingPlayers, 
+                winner : winner
+            })
+        } else {
+              io.sockets.emit('stick', { playerName: data.PlayerName })
+        }
+    })
+            //     send thru playernames and scores and winnerName
+
+            // browser will reveal cards, display scores, and name of  winner
+          
+            // change dealer, reveal 'play again?' button
+            // if pressed, send a begin event.
+        
+      
+    
+   
+
+
+
+// bottom of socket connection - everything inside here
+})
+
+
+
+
+
---
+++ public/chat.js
@@ -0,0 +1,235 @@
+// Make connection
+const socket = io.connect('http://localhost:4000')
+
+//Query DOM
+    //chat
+const message = document.querySelector('#message')
+const handle = document.querySelector('#handle')
+const send = document.querySelector('#send')
+const output = document.querySelector('#output')
+const feedback = document.querySelector('#feedback')
+
+    //blackjack
+const start = document.querySelector('#start-btn')
+const nameInput = document.querySelector('#name')
+const score = document.querySelector('#my-score')
+const opponentCards = document.querySelector('#opponent')
+const playerCards = document.querySelector('#player')
+const options = document.querySelector('#options')
+const stick = document.querySelector('#stick')
+const twist = document.querySelector('#twist')
+
+
+
+
+
+// Emit events
+
+send.addEventListener('click', () => {
+    socket.emit('chat',{
+        message: message.value,
+        handle: nameInput.value
+    })
+  message.value = ''
+})
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', nameInput.value)
+})
+
+// Listen for events
+socket.on('chat', data => {
+    feedback.innerHTML =''
+    output.innerHTML += `<p><strong>${data.handle}</strong> ${data.message}</p>`
+})
+
+socket.on('typing', data => {
+    feedback.innerHTML = `<p><em>${data} is typing a wee message...</em></p>`
+})
+
+
+// blackjack logic
+
+let dealer = true
+let playerName
+let currentScore = 0
+let playerWins = 0
+let opponentWins = 0
+    
+start.addEventListener('click', () => {
+    // hide button
+    setTimeout( () => {
+        start.classList.add('hidden')
+        
+        nameInput.classList.add('hidden')
+
+        score.innerText = 'waiting for other player to start...'
+
+    }, 500)
+
+    // send a startGame event to the server
+
+    playerName = nameInput.value
+    socket.emit('start', { playerName })
+
+})
+
+// handle start event responses from server
+socket.on('waiting', data => {
+        score.innerText = `${data.opponent} is ready to play!`
+        dealer = false
+})
+
+socket.on('begin', data => {
+    console.log(data)
+    score.innerText = `Lets Play Blackjack!`
+      setTimeout( () => {
+            dealCards(data)
+        },1000)
+
+        if (dealer) {
+             setTimeout( () => {
+            options.classList.remove('hidden')
+        },2500)
+        }
+       
+        // another delay, then if dealer unhide stick/twist buttons. message for other player e.g. player name's turn
+        // twist can be handled by browser
+             
+
+
+
+        //stick triggers an event to server
+            // on stick event server sends a endTurn event that gives stick/twist options to other player
+            // and displays a message to the first player
+})
+
+function createCard(cardData) {
+    let card = document.createElement('div')
+        card.classList.add('card')
+        card.innerText = cardData
+        return card
+}
+
+function dealCards(data) {
+    //create a div w/ class=card for first card in player array (transition in from below)
+    //create a div w/ class=card for first card in dealer array (transition in from above)
+    //repeat for 2nd cards
+
+    if (dealer) {
+        for (i=0;i<2;i++){          
+            opponentCards.appendChild(createCard(data.playerHand[i]))
+            playerCards.appendChild(createCard(data.dealerHand[i]))
+        }
+        currentScore = parseInt(data.dealerHand[0].replace(/\D/g, ""))
+         + parseInt(data.dealerHand[1] .replace(/\D/g, ""))
+        
+    } else {
+        for (i=0;i<2;i++){
+            playerCards.appendChild(createCard(data.playerHand[i]))
+            opponentCards.appendChild(createCard(data.dealerHand[i]))
+        }
+        currentScore = parseInt(data.playerHand[0].replace(/\D/g, "")) 
+        + parseInt(data.playerHand[1] .replace(/\D/g, ""))
+    }
+    score.innerText = `your score: ${currentScore}`
+}
+
+// stick or twist logic
+
+            /*twist function:
+                pings the server and receives a card back
+                    sends name to server. 
+                    server sneds back name with card. 
+                    browser checks if name returned === playername
+                
+                calls createCard, passing in new card.
+                appends the card to the players hand
+                recalculates the score and displays it
+                checks win/lose conditions
+            */
+
+twist.addEventListener('click', () => {
+    socket.emit('twist', { playerName })
+})
+
+// receiving a twist event
+socket.on('twist', data => {
+    if (data.playerName === playerName) {
+        let newCard = createCard(data.twistCard[0])
+        playerCards.appendChild(newCard)
+
+        let cardVal = parseInt(data.twistCard[0].replace(/\D/g, ""))
+
+        currentScore += cardVal
+        score.innerText = `Your Score: ${currentScore}`
+
+// separate win/lose conditions to another fuction bcs will have to be called a lot
+// need to run on first deal as well as each twist
+
+        if (playerCards.length === 2 && currentScore === 21) {
+            alert('Blackjack!')
+            playerWins++
+            // game restarts with other person as 'dealer'
+            // send a gameOver event to server with playerName, score, win: true/false
+        }
+
+        if (playerCards.length === 5 && currentScore <22) {
+            alert('Five Card Trick!')
+            playerWins++
+            // game restarts with other person as 'dealer'
+            // i thik the server should be keeping track of the scores??  
+        }
+
+        if (currentScore > 21) {
+            alert ('You are bust!')
+            currentScore = 0
+            socket.emit('stick', {
+                playerName, currentScore
+            })
+            // hide stick/twist buttons
+            setTimeout( () => {
+                options.classList.add('hidden')
+            },500)
+            opponentWins++
+            // game restarts
+        }
+
+
+    }
+})
+
+// send stick event to server with name, score
+stick.addEventListener( 'click', () => {
+    socket.emit('stick', {
+        playerName, currentScore
+    })
+    // hide stick/twist buttons
+    setTimeout( () => {
+        options.classList.add('hidden')
+    },500)
+})
+
+// handling a received stick event
+    socket.on('stick', data => {
+        if ( data.playerName !== playerName) {  // should I transmit form the server to avoid this check?  think about adapting for multiplayer!
+                                                // name of transmitted even can be diferent e.g. oppStick...?
+            // unhide stick/twist buttions
+            options.classList.remove('hidden')
+            
+        }
+    })
+
+    socket.on('gameOver', data => {
+        console.log(data)
+        let winMessage = `
+        ${data.scores[0].playerName} scored ${data.scores[0].currentScore},
+        ${data.scores[1].playerName} scored ${data.scores[1].currentScore}.
+        ${data.winner} is the winner!
+        `
+
+        score.innerText = winMessage
+    })
+      
+
+
